4698be4f067b86d1259a482f5e9404240dd4ec80
Samarth Goyal <samarth.goyal1999@gmail.com>
2023-10-17 18:36:25 +0530

    minor fixes

--- components/mainMessage.tsx
+++ components/mainMessage.tsx
@@ -9,7 +9,7 @@ export default function MainMessage(props:{props:PassingProps}) {
         <Draggable>
             <div style={{ marginLeft: `${Math.floor(Math.random() * (1300))}px`, marginTop: `${Math.floor(Math.random() * (600))}px`, animationDelay: `${props.props.index * 100}ms` }} className={`flex flex-col gap-4 absolute note bg-[#63B7B7]/50 backdrop-blur-sm w-[200px] h-[208px] text-center rounded-lg duration-200 p-4 cursor-grab active:cursor-grabbing select-none`}>
                 <p className='bg-[#63B7B7]/50 rounded line-clamp-1'>{props.props.message}</p>
-                <p className='bg-[#63B7B7]/50 rounded text-justify p-2'>{props.props.message2.substring(0, 90)}.</p>
+                <p className='bg-[#63B7B7]/50 rounded text-justify p-2 line-clamp-5'>{props.props.message2}.</p>
             </div>
         </Draggable>
         </>
